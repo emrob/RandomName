6c45514a2a6fcf97cc255e7ae65b3352f243175c
Emma Roberts <emrsixtyseven@gmail.com>
2018-03-19 20:40:05 +0000

    first commit

---
+++ src/main/java/RandomName.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class RandomName {
+
+    private ArrayList<String> names;
+
+    public RandomName(ArrayList<String> names) {
+        this.names = names;
+        this.names.add("Emma");
+        this.names.add("Derek");
+        this.names.add("Mark");
+        this.names.add("Darrell");
+        this.names.add("Rosemary");
+        this.names.add("Will");
+        this.names.add("Herbie");
+    }
+
+    public ArrayList<String> getNames() {
+        return names;
+    }
+
+    public void setNames(ArrayList<String> names) {
+        this.names = names;
+    }
+
+    public int getNumberOfNames() {
+        return names.size();
+    }
+
+    public String returnRandomName() {
+        Collections.shuffle(names);
+        return names.get(0);
+    };
+
+
+
+}
+
+
+
